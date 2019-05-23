3f1bc01b5322319f25965b6c75ee52b908c4da13
septoon <tigran.darchinyan@gmail.com>
2019-05-24 00:09:28 +0300

    start

---
+++ src/js/app.js
@@ -0,0 +1,35 @@
+const btn = document.getElementById('btn');
+const wrapper = document.getElementById('main');
+btn.addEventListener('click', () => {
+  btn.classList.add('hideBtn');
+  btn.classList.remove('activeBtn');
+
+  const chat = document.createElement('div');
+  chat.className = 'chat-wrapper activeChat';
+
+  chat.innerHTML = `<div class="chat-header">
+      <span class="title">Напишите нам</span>
+      <img src="./img/close.png" id="close" class="close" alt="close">
+    </div>
+    <div class="form_holder">
+      <form action="">
+        <textarea class="textarea" rows="10"></textarea>
+        <input class="send" type="submit">
+      </form>
+    </div>`;
+
+  wrapper.appendChild(chat);
+
+  const closeBtn = document.getElementById('close');
+
+  closeBtn.addEventListener('click', () => {
+    closeBtn.parentElement.parentElement.classList.add('hideChat');
+
+    setTimeout(() => {
+      closeBtn.parentElement.parentElement.remove();
+    }, 1000);
+
+    btn.classList.remove('hideBtn');
+    btn.classList.add('activeBtn');
+  });
+});
